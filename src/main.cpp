458d06a0036badb88ce4fa80d2045855065fd5a4
Valentin Veluppillai <valentinveluppillai@gmail.com>
2024-04-19 15:15:58 +0200

    level 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
+#include <map>
 #include "ccc_suite.h"
 
 void solution(istream &in, ostream &out) {
-    cout << "Hello World!\n";
 
     string s;
-    while(!in.eof()){
-        in >> s;
-        out << s;
+    getline(in, s);
+    while (!getline(in, s).eof()) {
+        map<char, int> totals;
+
+        for (char c : s) {
+            totals[c]++;
+        }
+
+        out << totals['W'] << " " << totals['D'] << " " << totals['S'] << " "
+            << totals['A'] << '\n';
     }
 }
