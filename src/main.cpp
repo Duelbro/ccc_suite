3f1f5cba7839030dc9b40e1267e6175d9a94c737
Duelbro <du3lbr0@gmail.com>
2024-04-19 15:31:39 +0200

    level 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,35 @@
 #include <map>
+
 #include "ccc_suite.h"
 
 void solution(istream &in, ostream &out) {
-
     string s;
     getline(in, s);
     while (!getline(in, s).eof()) {
-        map<char, int> totals;
+        int max_x = 0, max_y = 0, min_x = 0, min_y = 0, x = 0, y = 0;
 
         for (char c : s) {
-            totals[c]++;
+            switch (c) {
+                case 'W':
+                    y--;
+                    break;
+                case 'D':
+                    x++;
+                    break;
+                case 'S':
+                    y++;
+                    break;
+                case 'A':
+                    x--;
+                    break;
+            }
+            max_x = max(max_x, x);
+            max_y = max(max_y, y);
+            min_x = min(min_x, x);
+            min_y = min(min_y, y);
+
         }
 
-        out << totals['W'] << " " << totals['D'] << " " << totals['S'] << " "
-            << totals['A'] << '\n';
+        out << max_x - min_x + 1 << " " << max_y - min_y + 1 << "\r\n";
     }
 }
