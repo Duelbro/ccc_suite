7b503f41cb2bcb2dc66824e093945d58e9ef2426
Valentin Veluppillai <valentinveluppillai@gmail.com>
2024-10-25 15:38:23 +0200

    up to the middle of 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,66 @@
+#include <vector>
 #include "ccc_suite.h"
 
-void solution(istream &in, ostream &out) {
-    cout << "Hello World!\n";
+vector<vector<int>> make_map(int n_full, int m_full, int desk_count) {
+    vector<vector<int>> map;
+    map.resize(n_full);
+    for (auto &line : map) {
+        line.resize(m_full);
+    }
 
-    string s;
+    int m_nice = m_full - (m_full % 3);
+
+    int id = 0;
+    for (int i = 0; i < n_full; i++) {
+        for (int j = 0; j < m_nice; j += 3) {
+            map[i][j] = ++id;
+            map[i][j + 1] = id;
+            map[i][j + 2] = id;
+            desk_count--;
+
+            if (desk_count <= 0) {
+                break;
+            }
+        }
+    }
+
+    int n_nice = n_full - (n_full % 3);
+    for (int i = 0; i < n_nice; i += 3) {
+        for (int j = m_nice; j < m_full; j++) {
+            map[i][j] = ++id;
+            map[i][j + 1] = id;
+            map[i][j + 2] = id;
+            desk_count--;
+
+            if (desk_count <= 0) {
+                break;
+            }
+        }
+    }
+
+
+    return map;
+}
+
+void print_map(vector<vector<int>> map, int n, int m, ostream &out) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            out << map[i][j] << ' ';
+        }
+        out << '\n';
+    }
+    out << '\n';
+}
+
+void solution(istream &in, ostream &out) {
+    int x = 0;
+    int y = 0;
+    int dc = 0;
+    in >> y;
     while(!in.eof()){
-        in >> s;
-        out << s;
+        in >> x;
+        in >> y;
+        in >> dc;
+        print_map(make_map(y, x, dc), y, x, out);
     }
 }
