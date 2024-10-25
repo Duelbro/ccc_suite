710fb41b6362aea3e72d7675531ef5933dd04fb8
Valentin Veluppillai <valentinveluppillai@gmail.com>
2024-10-25 16:08:26 +0200

    level 4

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <cmath>
+#include <cassert>
 #include <vector>
 #include "ccc_suite.h"
 
@@ -8,16 +10,17 @@ vector<vector<int>> make_map(int n_full, int m_full, int desk_count) {
         line.resize(m_full);
     }
 
-    int m_nice = m_full - (m_full % 3);
+    int horizontal_tables = floor(((m_full+1)/4.0));
 
     int id = 0;
-    for (int i = 0; i < n_full; i++) {
-        for (int j = 0; j < m_nice; j += 3) {
-            map[i][j] = ++id;
-            map[i][j + 1] = id;
-            map[i][j + 2] = id;
+    for (int i = 0; i < n_full; i += 2) {
+        int j = 0;
+        for (int th = 0; th < horizontal_tables; th++) {
+            map[i][j++] = ++id;
+            map[i][j++] = id;
+            map[i][j++] = id;
+            j++;
             desk_count--;
-
             if (desk_count <= 0) {
                 break;
             }
@@ -28,12 +31,14 @@ vector<vector<int>> make_map(int n_full, int m_full, int desk_count) {
         return map;
     }
 
-    int n_nice = n_full - (n_full % 3);
-    for (int i = 0; i < n_nice; i += 3) {
-        for (int j = m_nice; j < m_full; j++) {
-            map[i][j] = ++id;
-            map[i + 1][j] = id;
-            map[i + 2][j] = id;
+    int vertical_tables = floor(((n_full + 1) / 4.0));
+    int i = 0;
+    for (int tv = 0; tv < vertical_tables; tv++) {
+        for (int j = 4 * horizontal_tables; j < m_full; j += 2) {
+            map[i++][j] = ++id;
+            map[i++][j] = id;
+            map[i++][j] = id;
+            i++;
             desk_count--;
 
             if (desk_count <= 0) {
@@ -42,6 +47,7 @@ vector<vector<int>> make_map(int n_full, int m_full, int desk_count) {
         }
     }
 
+    assert(desk_count == 0 && ("Could not place all desks!"));
 
     return map;
 }
@@ -49,7 +55,7 @@ vector<vector<int>> make_map(int n_full, int m_full, int desk_count) {
 void print_map(vector<vector<int>> map, int n, int m, ostream &out) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            out << map[i][j] << ' ';
+            out << (map[i][j] == 0 ? '.' : 'X');
         }
         out << '\n';
     }
@@ -69,7 +75,7 @@ void solution(istream &in, ostream &out) {
         if (in.eof()) {
             break;
         }
-        
+
         print_map(make_map(y, x, dc), y, x, out);
     }
 }
